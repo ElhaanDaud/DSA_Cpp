c53501ab7c54006a0dd79d71c48a2ad70a2c81a0
Abdullah-2309 <abdkhan2309@gmail.com>
2024-05-24 14:57:58 +0530

    Depth First Search

---
+++ dfs.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFSUtil(int v, vector<bool>& visited, vector<vector<int>>& adj) {
+    visited[v] = true;
+    cout << v << " ";
+
+    for (int i : adj[v]) {
+        if (!visited[i])
+            DFSUtil(i, visited, adj);
+    }
+}
+
+void DFS(int V, vector<vector<int>>& adj) {
+    vector<bool> visited(V, false);
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i])
+            DFSUtil(i, visited, adj);
+    }
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+
+    adj[0] = {1, 2};
+    adj[1] = {0, 3, 4};
+    adj[2] = {0};
+    adj[3] = {1};
+    adj[4] = {1};
+
+    cout << "Depth First Traversal: ";
+    DFS(V, adj);
+
+    return 0;
+}
