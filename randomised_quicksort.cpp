75c4c2859697496a92650c68c4e4d1eae8aabfd7
Abdullah-2309 <abdkhan2309@gmail.com>
2024-05-24 14:53:55 +0530

    Randomised Quicsort

---
+++ randomised_quicksort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+int randomizedPartition(vector<int>& arr, int low, int high) {
+    int randomPivot = low + rand() % (high - low + 1);
+    swap(arr[randomPivot], arr[high]);
+    return partition(arr, low, high);
+}
+
+void randomizedQuicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = randomizedPartition(arr, low, high);
+
+        randomizedQuicksort(arr, low, pi - 1);
+        randomizedQuicksort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    srand(time(0));
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+
+    cout << "Original array: ";
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+
+    randomizedQuicksort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
